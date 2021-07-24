7dfa6ce8783350596223a49acc660a415730a0e2
Bhavesh7393 <78908736+Bhavesh7393@users.noreply.github.com>
2021-07-24 17:46:22 +0530

    Update light_blocker_transform_HDA.py

--- light_blocker_transform_HDA.py
+++ light_blocker_transform_HDA.py
@@ -1,5 +1,5 @@
 '''
-select Arnold Light/s and run the script, it will generate a Light Blocker per light,
+select Arnold Light/s and run the script, it will generate a Light Blocker for all selected lights,
 and automatically assign OUT_light in the respective light's light_filter
 
 if you select nothing and run the script, it will still generate a Light Blocker,
@@ -22,11 +22,11 @@ if len(arnold_lights) == 0:
     light_blocker_transform = obj.createNode('light_blocker_transform')
     hou.Node.setSelected(light_blocker_transform, True)
 else:
+    light_blocker_transform = obj.createNode('light_blocker_transform')
     for light in arnold_lights:
-        light_blocker_transform = obj.createNode('light_blocker_transform', light.name()+'_light_blocker_transform1')
+        hou.parm(light.path()+'/ar_light_filters').set(light_blocker_transform.path()+"/matnet1/light_blocker1/OUT_light")
         light_blocker_transform_nodes.append(light_blocker_transform)
         hou.Node.setSelected(light_blocker_transform, True)
-        hou.parm(light.path()+'/ar_light_filters').set(light_blocker_transform.path()+"/matnet1/light_blocker1/OUT_light")
 
 if len(light_blocker_transform_nodes) != 0:
     hou.node('/obj').layoutChildren(items=(light_blocker_transform_nodes))
